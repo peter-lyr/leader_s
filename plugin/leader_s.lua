dee62b11f67cb394fb891540ae7964fd31544a22
peter-lyr <llydrp.ldp@gmail.com>
2024-09-16 22:51:26 +0800

    ['^<leader^>se'] require 'f'.telescope_extras()

--- plugin/leader_s.lua
+++ plugin/leader_s.lua
@@ -1,6 +1,7 @@
 require 'which-key'.register {
   ['<leader>s'] = { name = 'telescope', },
   ['<leader>sh'] = { '<cmd>Telescope help_tags<cr>', 'help_tags', mode = { 'n', 'v', }, },
-  ['<leader>sa'] = { '<cmd>Telescope builtin<cr>', 'builtin', mode = { 'n', 'v', }, },
+  ['<leader>sb'] = { '<cmd>Telescope builtin<cr>', 'builtin', mode = { 'n', 'v', }, },
+  ['<leader>se'] = { function() require 'f'.telescope_extras() end, 'builtin', mode = { 'n', 'v', }, },
   ['<leader>s<leader>'] = { '<cmd>Telescope file_browser<cr>', 'file_browser', mode = { 'n', 'v', }, },
 }
