994752c1e14046fecb478df2ec36bb57bc696de8
peter-lyr <llydrp.ldp@gmail.com>
2024-10-01 14:51:03 +0800

    <leader>s<leader> F.telescope_sel(F.get_cur_proj_dirs(), 'fd')

--- plugin/leader_s.lua
+++ plugin/leader_s.lua
@@ -9,14 +9,14 @@ require 'which-key'.register {
   ['<leader>sh'] = { '<cmd>Telescope help_tags<cr>', 'help_tags', mode = { 'n', 'v', }, },
   ['<leader>sa'] = { '<cmd>Telescope builtin<cr>', 'builtin', mode = { 'n', 'v', }, },
 
-  ['<leader>s<leader>'] = { '<cmd>Telescope file_browser<cr>', 'file_browser', mode = { 'n', 'v', }, },
-  ['<leader>su'] = { function() F.fd_sel(F.get_cur_proj_dirs()) end, 'fd_sel', mode = { 'n', 'v', }, },
+  ['<leader>sw'] = { '<cmd>Telescope file_browser<cr>', 'file_browser', mode = { 'n', 'v', }, },
+  ['<leader>s<leader>'] = { function() F.telescope_sel(F.get_cur_proj_dirs(), 'fd') end, 'fd_sel', mode = { 'n', 'v', }, },
 
   ['<leader>sl'] = { '<cmd>Telescope live_grep<cr>', 'live_grep', mode = { 'n', 'v', }, },
-  ['<leader>s<localleader>l'] = { function() F.live_grep_sel(F.get_cur_proj_dirs()) end, 'live_grep_sel', mode = { 'n', 'v', }, },
+  ['<leader>s<localleader>l'] = { function() F.telescope_sel(F.get_cur_proj_dirs(), 'live_grep') end, 'live_grep_sel', mode = { 'n', 'v', }, },
 
   ['<leader>ss'] = { '<cmd>Telescope grep_string<cr>', 'grep_string', mode = { 'n', 'v', }, },
-  ['<leader>s<localleader>s'] = { function() F.grep_string_sel(F.get_cur_proj_dirs()) end, 'grep_string_sel', mode = { 'n', 'v', }, },
+  ['<leader>s<localleader>s'] = { function() F.telescope_sel(F.get_cur_proj_dirs(), 'grep_string') end, 'grep_string_sel', mode = { 'n', 'v', }, },
 
   ["<leader>s'"] = { '<cmd>Telescope grep_string<cr>', 'grep_string', mode = { 'n', 'v', }, },
   ['<leader>sb'] = { '<cmd>Telescope buffers<cr>', 'buffers', mode = { 'n', 'v', }, },
