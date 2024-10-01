c75796bb1674f00e5342b43581bcec2f807a453a
peter-lyr <llydrp.ldp@gmail.com>
2024-10-01 14:53:13 +0800

    ['<leader>s<localleader>'] = { name = 'telescope.more', },

--- plugin/leader_s.lua
+++ plugin/leader_s.lua
@@ -6,6 +6,8 @@ require 'which-key'.register {
 
 require 'which-key'.register {
   ['<leader>s'] = { name = 'telescope', },
+  ['<leader>s<localleader>'] = { name = 'telescope.more', },
+
   ['<leader>sh'] = { '<cmd>Telescope help_tags<cr>', 'help_tags', mode = { 'n', 'v', }, },
   ['<leader>sa'] = { '<cmd>Telescope builtin<cr>', 'builtin', mode = { 'n', 'v', }, },
 
