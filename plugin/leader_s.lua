8442f4294f51a718d886588d5a2d8d7dfae76467
peter-lyr <llydrp.ldp@gmail.com>
2024-10-17 14:17:34 +0800

    <leader>sw F.cmd('Telescope file_browser cwd=%s', F.get_parent())

--- plugin/leader_s.lua
+++ plugin/leader_s.lua
@@ -15,7 +15,9 @@ require 'which-key'.register {
   ['<leader>sh'] = { '<cmd>Telescope help_tags<cr>', 'help_tags', mode = { 'n', 'v', }, },
   ['<leader>sa'] = { '<cmd>Telescope builtin<cr>', 'builtin', mode = { 'n', 'v', }, },
 
-  ['<leader>sw'] = { '<cmd>Telescope file_browser<cr>', 'file_browser', mode = { 'n', 'v', }, },
+  ['<leader>sw'] = { function() F.cmd('Telescope file_browser cwd=%s', F.get_parent()) end, 'file_browser cur', mode = { 'n', 'v', }, },
+  ['<leader>s<leader>w'] = { function() F.telescope_sel(F.get_cur_proj_dirs(), 'file_browser') end, 'file_browser sel', mode = { 'n', 'v', }, },
+
   ['<leader>s<leader><leader>'] = { function() F.telescope_sel(F.get_cur_proj_dirs(), 'fd') end, 'fd_sel', mode = { 'n', 'v', }, },
   ['<leader>sj'] = { function() F.opened_proj_sel() end, 'opened_proj_sel', mode = { 'n', 'v', }, },
 
@@ -35,7 +37,7 @@ require 'which-key'.register {
   ['<leader>sgh'] = { '<cmd>Telescope git_branches<cr>', 'git_branches', mode = { 'n', 'v', }, },
   ['<leader>sg<leader>h'] = { function() F.telescope_sel(F.get_cur_proj_dirs(), 'git_branches') end, 'git_branches_sel', mode = { 'n', 'v', }, },
 
-  ['<leader>sb'] = { function() F.cmd('Telescope buffers cwd=%s', F.get_cwd()) end, 'buffers', mode = { 'n', 'v', }, },
-  ['<leader>s<leader>b'] = { '<cmd>Telescope buffers<cr>', 'buffers', mode = { 'n', 'v', }, },
+  ['<leader>sb'] = { function() F.cmd('Telescope buffers cwd=%s', F.get_cwd()) end, 'buffers cwd', mode = { 'n', 'v', }, },
+  ['<leader>s<leader>b'] = { '<cmd>Telescope buffers<cr>', 'buffers all', mode = { 'n', 'v', }, },
   ['<leader>sp'] = { '<cmd>Telescope project<cr>', 'project', mode = { 'n', 'v', }, },
 }
